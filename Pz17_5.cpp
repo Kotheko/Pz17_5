e48de302199214afb502d9e9f1181cf462648fa0
Ruslan Denisov <necronlive@mail.ru>
2024-01-21 20:01:39 +0900

    Initial

---
+++ Pz17_5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+class Vector
+{
+public:
+    Vector() : x(1), y(1), z(1)
+    {}
+
+    Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+    {}
+
+    double modul()
+    {
+        return std::sqrt(x * x + y * y + z * z);
+    }
+
+private:
+    double x;
+    double y;
+    double z;
+};
+
+int main()
+{
+    Vector v(4, 8, 5);
+
+    std::cout << v.modul();
+}
